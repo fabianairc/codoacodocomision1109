5a712d94edaa40fa4c67932bd14c79e86f29d02f
fabianairc <fabianaramirezzz@gmail.com>
2021-09-21 17:54:54 -0300

    js

---
+++ JAVASCRIPT/jS/app.js
@@ -0,0 +1,20 @@
+alert("Hola Mundo");
+console.log( "estamos en la consol");
+let nombre = "Fabiana";
+var numero  = 10;
+const Pi = 3.1416;
+let Hayclasses= true;
+console.log((numero+25) + nombre);
+if (5>3){
+console.log("es correcto")};
+else {
+    console.log("no es correcto")
+};
+console.log ((5>3)? "es correcto 5>3":"no es correcto");
+else{
+    console.log("no me conecto a la videollamada")
+},
+if(Hay clases){console.log ("me conecto a la videollamada")}
+let element = document.getElementById("principal");
+console.log (element)
+element.innerHTML "<H1> "
